dc38011730fe7fc683db3bcb75473812e80bc271
Jordy Decena <jordy.dcna@gmail.com>
2019-03-16 15:01:12 -0400

    Initial commit

---
+++ app/src/main/java/joscar/soft/lidomlive/MainActivity.java
@@ -0,0 +1,58 @@
+package joscar.soft.lidomlive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView Browser;
+    Timer Temp;
+    int Counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Browser = findViewById(R.id.Browser);
+        Browser.setWebViewClient(new WebViewClient());
+
+        WebSettings BrowserSettings = Browser.getSettings();
+        BrowserSettings.setJavaScriptEnabled(true);
+
+        SetLoop();
+    }
+
+    private void SetLoop(){
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(seText);
+            }
+        };
+
+        Temp = new Timer();
+        Temp.schedule(task, 1000,15000);
+    }
+
+
+
+    final Runnable seText = new Runnable() {
+        @Override
+        public void run() {
+            Counter++;
+            TextView Indicator = findViewById(R.id.LabelIndicator);
+            Browser.loadUrl("https://www.licey.com/wp-content/plugins/licey-gameday/liceygameday_new.html");
+            Indicator.setText("Running task #" + Counter);
+        }
+    };
+}
